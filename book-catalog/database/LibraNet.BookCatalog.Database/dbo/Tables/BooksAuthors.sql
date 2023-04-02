f39fe573a3798202928b1469da96dfd11bc07e3b
Laura Kolčavová <laura.kolcavova@notino.com>
2023-04-02 17:51:46 +0200

    feat: Prepare database project for BookCatalog

---
+++ book-catalog/database/LibraNet.BookCatalog.Database/dbo/Tables/BooksAuthors.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BooksAuthors]
+(
+	[BookId] INT NOT NULL,
+	[AuthorId] INT NOT NULL,
+	CONSTRAINT [PK_BooksAuthors] PRIMARY KEY CLUSTERED ([BookId] ASC, [AuthorId] ASC),
+	CONSTRAINT [FK_BooksAuthors_Books] FOREIGN KEY ([BookId]) REFERENCES [dbo].[Books] ([Id]),
+	CONSTRAINT [FK_BooksAuthors_Authors] FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[Authors] ([Id])
+)
